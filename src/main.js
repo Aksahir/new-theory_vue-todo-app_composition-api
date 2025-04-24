edfa9c86ccc108b056a2451b65320185b3dcf67f
Александр <0999353523ak@gmail.com>
2025-04-24 15:32:01 +0300

    add final

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import './assets/main.css'
+// import './assets/main.css'
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './assets/styles/index.scss';
 
 import { createApp } from 'vue'
 import App from './App.vue'
